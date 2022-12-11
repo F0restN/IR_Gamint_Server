ebb2ef5ef10b83e5045b6e8a1a0a5d46f7c575d4
Collin2046 <keh229@pitt.edu>
2022-12-10 21:15:21 -0500

    feat: stopwords constructe

--- src/main/java/system/classes/Path.java
+++ src/main/java/system/classes/Path.java
@@ -3,4 +3,6 @@ package system.classes;
 public class Path {
     public static String dataPath="data//resDoc.txt";
     public static String IndexData="data//indexData";
+
+    public static String stopWord = "data//stopWord.txt";
 }
