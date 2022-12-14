1e5495ed1c13d8b3d51638eb47b326384a3256b1
Collin2046 <keh229@pitt.edu>
2022-12-14 17:48:21 -0500

    fix : delete rank main

--- src/main/java/system/utils/Rank.java
+++ src/main/java/system/utils/Rank.java
@@ -17,12 +17,4 @@ public class Rank {
         }
         return Rank;
     }
-
-    public static void main(String[] args) {
-        String a = "lizard";
-        String [] arr= a.split(" ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(i + " "+ arr[i]);
-        }
-    }
 }
